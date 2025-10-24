c0ad40d293871f30d377039b0ccf724e41a00459
Liam Miller <l.miller2@universityofgalway.com>
2025-10-24 16:54:19 +0100

    Fixingdataset

--- datasets/prepare_dataset.py
+++ datasets/prepare_dataset.py
@@ -3,10 +3,9 @@ import pandas as pd
 input_file = "IoTsec-Room-Climate-Datasets.txt"
 output_file = "sim_data.csv"
 
-# Read the 3-column dataset (timestamp, RH%, Temp °C)
 df = pd.read_csv(input_file, header=None, names=["timestamp_ms", "rh_pct", "temp_c"])
 
-# Keep only the columns your firmware expects
 df[["temp_c", "rh_pct"]].to_csv(output_file, index=False)
+
 
 print(f"Saved dataset to: {output_file}")
